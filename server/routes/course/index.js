ebf50a02d27e60df92fbd07c54c2dd86416bebc6
Daniel Almasi <almd@pm.me>
2020-04-25 19:18:38 +0100

    Add pagination to frontend
    Add pagination to frontend

--- server/routes/course/index.js
+++ server/routes/course/index.js
@@ -29,12 +29,21 @@ router.get('/', async (req, res) => {
         const filters = req.query;
         const fetchResult = await courseService.fetchByFilters(filters);
         const pageData = pagination.getPageData(req.query.currentPage, req.query.pageSize, fetchResult);
+        const searchUrl = req.baseUrl + "?" +
+            "capability=" + (req.query.capability ? req.query.capability : "-1") + "&" +
+            "category=" + (req.query.category ? req.query.category : "-1") + "&" +
+            "competency=" + (req.query.competency ? req.query.competency : "-1") + "&" +
+            "phase=" + (req.query.phase ? req.query.phase : "-1") + "&" +
+            (req.query.keyword ? `keyword=${req.query.keyword}&` : "") +
+            (req.query.pageSize ? `pageSize=${req.query.pageSize}&` : "") +
+            "currentPage=";
         log.info("Fetched & resolved %s courses, returning page %s of %s",
             fetchResult.length, pageData.meta.currentPage, pageData.meta.pageCount);
         res.render('courses.ejs', {
             courses: fetchResult,
             pageData,
             baseurl: req.baseUrl, // TODO: ?
+            searchUrl,
         });
     } catch (error) {
         log.error("Failed fetching courses with %s filters, err: " + error.message, req.query);
