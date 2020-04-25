55f0b7101405519599046fd7b952ab7134f9e329
Daniel Almasi <almd@pm.me>
2020-04-25 12:38:30 +0100

    Considerably increase page load times by localising dependencies
    Considerably increase page load times by localising dependencies

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ const path = require('path');
 // -- MIDDLEWARE -- //
 app.set('view-engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
