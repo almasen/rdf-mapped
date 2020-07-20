eaf8bfcbb0c52d9b0735a0a45e39ffdb53ba665b
Daniel Almasi <almd@pm.me>
2020-07-20 23:30:29 +0100

    Implement JWE verification for admin panel
    Implement JWE verification for admin panel

--- server/routes/admin/panel/index.js
+++ server/routes/admin/panel/index.js
@@ -4,28 +4,38 @@ const log = require("../../../util/log");
 const courseService = require("../../../modules/course");
 const videoService = require("../../../modules/video");
 const submissionService = require("../../../modules/submission");
+const adminService = require("../../../modules/admin");
 
 router.get('/', async (req, res) => {
-    // log.info("Rendering admin log-in page..");
     try {
-        const courses = await courseService.fetchAll();
-        const uniqueCourses = await courseService.fetchAllWithUniqueTitles();
-        const videos = await videoService.fetchAll();
-        const uniqueVideos = await videoService.fetchAllWithUniqueTitles();
-        const submissions = await submissionService.fetchAll();
-        res.render("admin-panel.ejs", {
-            baseurl: req.baseUrl,
-            courses,
-            uniqueCourses,
-            videos,
-            uniqueVideos,
-            submissions,
-        });
+        const jwe = req.cookies.jwe;
+        if (jwe) {
+            log.info("Attempting to authenticate a user for admin panel, ref:'%s'", jwe.split('.')[4]);
+            const adminName = adminService.authenticateAdmin(jwe);
+            log.info("Successfully authenticated %s for admin panel, ref:" + jwe.split('.')[4], adminName);
+
+            // Fetch admin site-statistics
+            const courses = await courseService.fetchAll();
+            const uniqueCourses = await courseService.fetchAllWithUniqueTitles();
+            const videos = await videoService.fetchAll();
+            const uniqueVideos = await videoService.fetchAllWithUniqueTitles();
+            const submissions = await submissionService.fetchAll();
+            res.render("admin-panel.ejs", {
+                baseurl: req.baseUrl,
+                adminName,
+                courses,
+                uniqueCourses,
+                videos,
+                uniqueVideos,
+                submissions,
+            });
+        } else {
+            log.info("An attempted visit at admin panel without a jwe token, redirecting to admin/login");
+            res.redirect("/admin/login");
+        }
     } catch (error) {
-        log.error("Failed rendering admin panel page, err: " + error.message);
-        res.status(404).render('404.ejs', {
-            baseurl: "",
-        });
+        log.error("Failed to authenticate for admin panel, err: " + error.message);
+        res.redirect("/admin/login");
     }
 });
 
