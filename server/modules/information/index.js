b3c3b01e06fbbca4daf0b569b7ce5095e95abb13
Daniel Almasi <almd@pm.me>
2020-07-21 17:32:56 +0100

    Update information module documentation
    Update information module documentation

--- server/modules/information/index.js
+++ server/modules/information/index.js
@@ -2,36 +2,24 @@ const informationRepository = require("../../repositories/information");
 
 /**
  * Updates an information entry that already exists in the database.
- * @param {object} information A valid information object
- * Fails if database calls fail.
+ * @param {Object} information a valid information object
+ * @return {Object} updated information entry
  */
 const updateInformation = async (information) => {
     const updateInformationResult = await informationRepository.update(information);
-    return ({
-        status: 200,
-        message: "Information entry updated successfully",
-        data: {information: updateInformationResult.rows[0]},
-    });
+    return updateInformationResult.rows[0];
 };
 
 /**
  * Gets data about an information entry that already exists in the database.
  * @param {String} type type of information
- * Fails if database calls fail.
+ * @return {String} content of information entry
  */
 const getInformationData = async (type) => {
     const informationResult = await informationRepository.findByType(type);
-    const information = informationResult.rows[0];
+    const informationObject = informationResult.rows[0];
 
-    return ({
-        status: 200,
-        message: "Information entry fetched successfully",
-        data: {
-            information: {
-                ...information,
-            },
-        },
-    });
+    return informationObject.content;
 };
 
 module.exports = {
