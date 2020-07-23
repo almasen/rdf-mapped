59af849384d721a839a5bbb023259f6d78a9cb0f
Daniel Almasi <almd@pm.me>
2020-07-23 23:14:23 +0100

    Refactor server to start after all services have been initialised
    Refactor server to start after all services have been initialised

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const log = require("./util/log");
 const app = require('./app');
+const startupService = require("./modules/startup");
 const PORT = process.env.PORT || 8000;
 
-
-app.listen(PORT, () => {
-    if (process.env.NODE_ENV !== "test") {
-        log.info(`Server started successfully. Listening on port ${PORT} ...`);
-    }
-});
+// Initialise all services then start server
+(async () => {
+    await startupService.initialise();
+    log.info(`Successfully initialised services. Starting server..`);
+    app.listen(PORT, () => {
+        log.info(`Server started successfully. Listening on port ${PORT}.`);
+    });
+})();
