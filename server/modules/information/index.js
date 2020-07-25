3368271443ecb1bdac9d7710286f9128de638822
Daniel Almasi <almd@pm.me>
2020-07-25 18:04:54 +0100

    Implement caching information records
    Implement caching information records

--- server/modules/information/index.js
+++ server/modules/information/index.js
@@ -1,25 +1,39 @@
 const informationRepository = require("../../repositories/information");
+const cache = require("../cache");
 
 /**
  * Updates an information entry that already exists in the database.
+ * Updates the record in the cache too.
  * @param {Object} information a valid information object
  * @return {Object} updated information entry
  */
 const updateInformation = async (information) => {
     const updateInformationResult = await informationRepository.update(information);
-    return updateInformationResult.rows[0];
+    const updatedRecord = updateInformationResult.rows[0];
+    // if successful, cache it
+    cache.set(`information-${information.type}`, updatedRecord.content);
+    return updatedRecord;
 };
 
 /**
- * Gets data about an information entry that already exists in the database.
+ * Gets data about an information entry that already exists in
+ * the cache or database. Caches the value if only in DB.
+ * Does not validate that DB has the record!
  * @param {String} type type of information
  * @return {String} content of information entry
  */
 const getInformationData = async (type) => {
-    const informationResult = await informationRepository.findByType(type);
-    const informationObject = informationResult.rows[0];
+    const cacheKey = `information-${type}`;
+    if (cache.has(cacheKey)) {
+        return cache.get(cacheKey);
+    } else {
+        const informationResult = await informationRepository.findByType(type);
+        const informationObject = informationResult.rows[0];
 
-    return informationObject.content;
+        cache.set(cacheKey, informationObject.content);
+
+        return informationObject.content;
+    }
 };
 
 module.exports = {
