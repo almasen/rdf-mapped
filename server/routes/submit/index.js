18bce74cb410fa1ebd7b924289233b18ec81a0d2
Daniel Almasi <almd@pm.me>
2020-08-03 22:14:18 +0100

    Add base64 to url safe base64 converter
    Add base64 to url safe base64 converter

--- server/routes/submit/index.js
+++ server/routes/submit/index.js
@@ -6,6 +6,10 @@ const categoryService = require("../../modules/category");
 const competencyService = require("../../modules/competency");
 const phaseService = require("../../modules/phase");
 const filtering = require("../../modules/filtering");
+const httpUtil = require("../../util/http");
+const downloadService = require("../../modules/download");
+const submissionService = require("../../modules/submission");
+const captchaService = require("../../modules/captcha");
 
 router.get('/', async (req, res) => {
     try {
@@ -23,12 +27,38 @@ router.get('/', async (req, res) => {
             phases,
             categoriesByParents,
             competenciesByParents,
+            reCAPTCHASiteKey: process.env.GOOGLE_RECAPTCHA_KEY,
         });
     } catch (error) {
         log.error("Failed rendering content submission page, err:" + error.message);
         res.status(404).render('404.ejs', {
             baseurl: "",
         });
+    }
+});
+
+router.post('/', async (req, res) => {
+    log.info("Submitting a new %s..", req.body.type);
+    try {
+        console.log(req.body);
+        const captchaVerified = await captchaService.verifyResponse(req.body['g-recaptcha-response']);
+        if (!captchaVerified) {
+            return httpUtil.sendResult({
+                status: 400,
+                message: "reCAPTCHA verification failed",
+            }, res);
+        } else {
+            const {type, title, hyperlink, email} = req.body;
+            await submissionService.insertNewSubmission(type, title, hyperlink, email);
+            downloadService.deleteExportFiles();
+            return httpUtil.sendResult({
+                status: 200,
+                message: "Successfully added a new " + req.body.type,
+            }, res);
+        }
+    } catch (error) {
+        log.error("Adding new %s failed, err: " + error.message, req.body.type);
+        return httpUtil.sendGenericError(error, res);
     }
 });
 
