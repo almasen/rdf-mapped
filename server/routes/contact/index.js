b7339dbef16bcbcb99adeaad5f5ab530ba029515
Daniel Almasi <almd@pm.me>
2020-08-08 18:45:34 +0100

    Add reCAPTCHA to contact form
    Add reCAPTCHA to contact form

--- server/routes/contact/index.js
+++ server/routes/contact/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const mail = require("../../modules/mail");
+const captchaService = require("../../modules/captcha");
 const log = require("../../util/log");
 
 router.get('/', (req, res) => {
     try {
         res.render("contact-us.ejs", {
             baseurl: req.baseUrl,
+            reCAPTCHASiteKey: process.env.GOOGLE_RECAPTCHA_KEY,
         });
     } catch (error) {
         log.error("Failed to render contact us page, err: " + error.message);
@@ -16,15 +18,23 @@ router.get('/', (req, res) => {
     }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     log.info("%s: Sending us contact-us message..", req.body.email);
     try {
-        mail.sendEmail(
-            process.env.CONTACT_EMAIL_ADDRESS,
-            process.env.CONTACT_EMAIL_ADDRESS,
-            `[rdfmapped.com] Contact form: ${req.body.subject}`,
-            `Contact request from ${req.body.email} - ${req.body.name}:\n${req.body.message}`,
-        );
+        const captchaVerified = await captchaService.verifyResponse(req.body['g-recaptcha-response']);
+        if (!captchaVerified) {
+            return httpUtil.sendResult({
+                status: 400,
+                message: "reCAPTCHA verification failed",
+            }, res);
+        } else {
+            mail.sendEmail(
+                process.env.CONTACT_EMAIL_ADDRESS,
+                process.env.CONTACT_EMAIL_ADDRESS,
+                `[rdfmapped.com] Contact form: ${req.body.subject}`,
+                `Contact request from ${req.body.email} - ${req.body.name}:\n${req.body.message}`,
+            );
+        }
     } catch (error) {
         log.error("%s: Contact-us failed, err: " + error.message, req.body.email);
     }
