87cae236cd6550e55e308d3d2ec7e2cf2ad953a5
Daniel Almasi <almd@pm.me>
2020-08-20 23:15:17 +0100

    Refactor bugreport module
    Refactor bugreport module

--- server/modules/bugreport/index.js
+++ server/modules/bugreport/index.js
@@ -1,6 +1,21 @@
 const mail = require("../mail");
 const adminService = require("../admin");
 const log = require("../../util/log");
+const octokit = require("../octokit");
+
+const processBugReport = async (originalUrl, ip, email, report) => {
+    log.info("Processing bug report for route: '%s'", originalUrl);
+    const bugreport = {
+        timestamp: (new Date()).toUTCString(),
+        originalUrl,
+        ip,
+        email,
+        report,
+    };
+    await sendBugReport(bugreport);
+    adminService.logBugReport(bugreport);
+    octokit.createBugreportIssue(bugreport);
+};
 
 /**
  * Send a bug report email asynchronously to the bug report
@@ -9,29 +24,17 @@ const log = require("../../util/log");
  * user's email is specified in the body of the email
  * if they provided one. Furthermore, the admin service
  * is called to log the bug report.
- * @param {string} originalUrl
- * @param {string} ip
- * @param {string} email if provided by the user
- * @param {string} report the user input bug report
+ * @param {Object} bugreport
  */
-const sendBugReport = async (originalUrl, ip, email, report) => {
-    log.info("Sending bug report for route: '%s'", originalUrl);
-    const bugreport = {
-        timestamp: (new Date()).toUTCString(),
-        originalUrl,
-        ip,
-        email,
-        report,
-    };
-    adminService.logBugReport(bugreport);
+const sendBugReport = async (bugreport) => {
     await mail.sendEmail(
         process.env.BUG_REPORT_EMAIL_ADDRESS,
         process.env.BUG_REPORT_EMAIL_ADDRESS,
-        `[rdfmapped.com] Bug Report for route: ${originalUrl}`,
+        `[rdfmapped.com] Bug Report for route: ${bugreport.originalUrl}`,
         `Bugreport: ${JSON.stringify(bugreport)}`,
         `<p>Bugreport: <code>${JSON.stringify(bugreport)}</code></p>`);
 };
 
 module.exports = {
-    sendBugReport,
+    processBugReport,
 };
