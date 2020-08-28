bd737cceeb54f07138e1cb7d2f5fa0f6256ec1b3
Daniel Almasi <almd@pm.me>
2020-08-28 12:51:24 +0100

    Add recache scheduling option to environment
    Add recache scheduling option to environment

--- server/modules/scheduler/index.js
+++ server/modules/scheduler/index.js
@@ -13,9 +13,14 @@ const adminService = require("../admin");
  */
 const scheduleRecache = () => {
     schedule.scheduleJob(config.recache, async () => {
-        log.info("SCHEDULER: Executing scheduled task 'recacheAll()' at %s...",
-            (new Date).toUTCString());
-        await recache.recacheAll();
+        if (process.env.SCHEDULE_RECACHE === 1) {
+            log.info("SCHEDULER: Executing scheduled task 'recacheAll()' at %s...",
+                (new Date).toUTCString());
+            await recache.recacheAll();
+        } else {
+            log.info("SCHEDULER: SKIPPING scheduled task 'recacheAll()' at %s...",
+                (new Date).toUTCString());
+        }
     });
 };
 
